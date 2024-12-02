acb09ee0da2f8c1a010e2719fd660ae5bd9b3b58
hugop <hugopessego@gmail.com>
2024-12-02 19:43:09 +0000

    1st version - cqrs pattern with projection.

---
+++ src/main/java/com/thedevhorse/cqrswithprojections/CqrsWithProjectionsApplication.java
@@ -0,0 +1,13 @@
+package com.thedevhorse.cqrswithprojections;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CqrsWithProjectionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CqrsWithProjectionsApplication.class, args);
+    }
+
+}
